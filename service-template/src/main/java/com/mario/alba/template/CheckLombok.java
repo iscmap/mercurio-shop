1d29538b771fd922ea2ea281b194616dae373143
Mario Alba <iscmap@gmail.com>
2026-01-22 11:53:38 -0600

    chore: add spotless formatting

---
+++ service-template/src/main/java/com/mario/alba/template/CheckLombok.java
@@ -0,0 +1,13 @@
+package com.mario.alba.template;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class CheckLombok {
+
+  int value;
+}
